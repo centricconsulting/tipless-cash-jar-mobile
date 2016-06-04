9f7d8ff3e34d26abeb4ccf841c3dee15d744acdd
Chris Martinez <chris.martinez@centricconsulting.com>
2016-06-04 17:55:21 -0400

    Add Account activity page

---
+++ TiplessCashJar/AccountActivityPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TiplessCashJar
+{
+	public class AccountActivityPage : ContentPage
+	{
+		ListView accountListView = new ListView();
+
+		public AccountActivityPage ()
+		{
+			Content = new StackLayout { 
+				Padding = new Thickness(20),
+				Children = {
+					accountListView
+				}
+			};
+		}
+	}
+}
+
+
