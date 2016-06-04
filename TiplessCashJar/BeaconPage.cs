c3961f8d0cc678532eaa3e6ab3ed0b8d03141c10
Chris Martinez <chris.martinez@centricconsulting.com>
2016-06-03 20:13:30 -0400

    Add BLE capability

---
+++ TiplessCashJar/BeaconPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TiplessCashJar
+{
+	public class BeaconPage : ContentPage
+	{
+		public BeaconPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
